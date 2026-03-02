f22f149c9c111d57f6e1f8e0975009bfa7187476
ISHAAN VAISH <224352959+ishaanvaish06@users.noreply.github.com>
2026-03-02 18:05:57 +0530

    Create Two_Sum.cpp

---
+++ Array/Two_Sum.cpp
@@ -0,0 +1,17 @@
+class Solution { //LEETCODE - TWO SUM
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++)
+        {
+            int diff=target-nums[i];
+            if(m.count(diff))
+            {
+                return {m[diff],i};
+            }
+            m[nums[i]]=i;
+        }
+        return {};
+
+    }
+};
