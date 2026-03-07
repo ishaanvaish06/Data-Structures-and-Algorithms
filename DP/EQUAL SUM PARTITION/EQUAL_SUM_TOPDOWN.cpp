ddac4f4725ff7236a77bd4bff3e6bff1e597fadd
ISHAAN VAISH <224352959+ishaanvaish06@users.noreply.github.com>
2026-03-07 11:05:08 +0530

    Create EQUAL_SUM_TOPDOWN.cpp

---
+++ DP/EQUAL SUM PARTITION/EQUAL_SUM_TOPDOWN.cpp
@@ -0,0 +1,34 @@
+bool t[101][101];
+bool knapsack(int arr[],int n,int sum) {
+    for (int i=0;i<n+1;i++) {
+        for (int j=0;j<sum+1;j++) {
+            if (i==0) {
+                t[i][j]=false;
+            }
+            if (j==0) {
+                t[i][j]=true;
+            }
+            else if (arr[i-1]<=j) {
+                t[i][j]=t[i-1][j-arr[i-1]] || t[i-1][j];
+            }
+            else {
+                t[i][j]=t[i-1][j];
+            }
+        }
+    }
+    return t[n][sum];
+}
+
+
+bool check(int arr[],int n) {
+    int sum=0;
+    for (int i=0;i<n;i++) {
+        sum +=arr[i];
+    }
+    if (sum%2==0) {
+        return knapsack(arr,n,sum/2);
+    }
+    else {
+        return false;
+    }
+}
