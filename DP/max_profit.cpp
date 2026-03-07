8e6d44f9e8cd30ec5e70a0a459204ebf20adc563
ISHAAN VAISH <224352959+ishaanvaish06@users.noreply.github.com>
2026-03-07 07:35:23 +0530

    dp

---
+++ DP/max_profit.cpp
@@ -0,0 +1,30 @@
+//
+// Created by ishaa on 07-03-2026.
+//
+
+#include "max_profit.h"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int knapsack(int wt[],int val[],int W,int n)
+{
+    if (n==0 || W==0) return 0;
+    if (wt[n-1]<=W) {
+        return max(val[n-1]+knapsack(wt,val,W-wt[n-1],n-1),knapsack(wt,val,W,n-1));
+    }
+    else
+    {
+        return knapsack(wt, val, W, n - 1);
+    }
+}
+
+int main() {
+    int wt[]={1,3,4,5};
+    int val[]={1,4,5,7};
+    int n=4;
+    int W=7;
+    int profit = knapsack(wt,val,W,n);
+    cout<<profit<<endl;
+    return 0;
+}
---
+++ DP/max_profit.h
@@ -0,0 +1,14 @@
+//
+// Created by ishaa on 07-03-2026.
+//
+
+#ifndef DP_MAX_PROFIT_H
+#define DP_MAX_PROFIT_H
+
+
+class max_profit
+{
+};
+
+
+#endif //DP_MAX_PROFIT_H
