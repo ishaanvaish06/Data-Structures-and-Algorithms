fda5ce2c7c310400f0e320b804521294e332501d
ISHAAN VAISH <224352959+ishaanvaish06@users.noreply.github.com>
2026-03-07 13:58:31 +0530

    duplacte sum

---
+++ Array/contains_duplicate.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(m[nums[i]]==0)
+            {
+                m[nums[i]]=1;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        return false;
+
+    }
+};
