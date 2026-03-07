375020d318987ec9e7dc6d56a51dc574c1792b4b
ISHAAN VAISH <224352959+ishaanvaish06@users.noreply.github.com>
2026-03-07 10:50:31 +0530

    subset_sum_recursive

--- DP/subset_sum_recursive.cpp
+++ DP/subset_sum_recursive.cpp
@@ -1,20 +1,18 @@
-bool t[101][101];
 bool knapsack(int arr[],int sum,int n) {
-    for (int i=0;i<n+1;i++) {
-        for (int j=0;j<sum+1;j++) {
-            if (i==0) {
-                t[i][j]=false;
-            }
-            if (j==0) {
-                t[i][j]=true;
-            }
-            else if (arr[i-1]<=j) {
-                t[i][j]=t[i-1][j-arr[i-1]] || t[i-1][j];
-            }
-            else {
-                t[i][j]=t[i-1][j];
-            }
-        }
+    if (n==0 && sum==0) {
+        return true;
     }
-    return t[n][sum];
+    else if (n==0)
+    {
+        return false;
+    }
+    else if (sum==0) {
+        return true;
+    }
+    else if (arr[n-1]<=sum) {
+        return knapsack(arr,sum-arr[n-1],n-1) || knapsack(arr,sum,n-1);
+    }
+    else if (arr[n-1]>sum) {
+        return knapsack(arr,sum,n-1);
+    }
 }
