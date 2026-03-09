5e711d2d97220ddebe90b9e3df1b21147d9dee94
ISHAAN VAISH <224352959+ishaanvaish06@users.noreply.github.com>
2026-03-09 16:54:26 +0530

    Create next_smaller_to_right.cpp

---
+++ STACK/next_smaller_to_right.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> a(vector<int>& arr) {
+    vector<int> res;
+    stack<int> s;
+    for (int i=arr.size()-1; i>=0; i--) {
+        if (s.empty()) {
+            res.push_back(-1);
+        }
+        else if (s.size()>0 && s.top()<arr[i]) {
+            res.push_back(s.top());
+        }
+        else if (s.size()>0 && s.top()>=arr[i]) {
+            while (s.size()>0 && s.top()>=arr[i]) {
+                s.pop();
+            }
+            if (s.size()==0) {
+                res.push_back(-1);
+            }
+            else {
+                res.push_back(s.top());
+            }
+        }
+        s.push(arr[i]);
+    }
+    reverse(res.begin(), res.end());
+    return res;
+}
