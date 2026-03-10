78d767e8f58e2f55aeee63e8dc4bb450d20d3c8f
ISHAAN VAISH <224352959+ishaanvaish06@users.noreply.github.com>
2026-03-10 16:50:20 +0530

    Create palindromic_linked_list.cpp

---
+++ STACK/palindromic_linked_list.cpp
@@ -0,0 +1,30 @@
+/**
+* Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode* temp=head;
+        stack<int> s;
+        while(temp!=NULL)
+        {
+            s.push(temp->val);
+            temp=temp->next;
+        }
+        ListNode *hello=head;
+        while(hello!=NULL && hello->val==s.top())
+        {
+            s.pop();
+            hello=hello->next;
+        }
+        return s.size()==0;
+
+    }
+};
