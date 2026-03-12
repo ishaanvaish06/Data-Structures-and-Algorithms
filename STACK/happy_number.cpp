bb28dbf63a17b2205aae7ef45d21535bf42d3c06
ISHAAN VAISH <224352959+ishaanvaish06@users.noreply.github.com>
2026-03-12 18:59:52 +0530

    Create happy_number.cpp

---
+++ STACK/happy_number.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int sq(int n)
+    {
+        int x=0;
+        while(n>0)
+        {
+            int y=(n%10)*(n%10);
+            x=x+y;
+            n=n/10;
+        }
+        return x;
+    }
+
+    bool isHappy(int n) {
+        unordered_map<int,int> m;
+        while(true)
+        {
+            if(m[n]==0 && n!=1)
+            {
+                m[n]=1;
+                n=sq(n);
+            }
+            else if(m[n]!=0 && n!=1)
+            {
+                return false;
+                break;
+            }
+            else if(n==1)
+            {
+                return true;
+                break;
+            }
+        }
+        
+
+
+    }
+
+};
