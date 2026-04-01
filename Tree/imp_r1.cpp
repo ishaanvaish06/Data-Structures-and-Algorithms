0f0e698012434564966b1c580dc6d1bd64abc466
ISHAAN VAISH <224352959+ishaanvaish06@users.noreply.github.com>
2026-04-01 18:16:18 +0530

    v2

---
+++ Tree/imp_r1.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node *left;
+    Node * right;
+    Node(int x) {
+        this->data = x;
+        right=left=NULL;
+    }
+};
+int idx=-1;
+Node* buildTr(vector<int> &v) {
+    idx++;
+    if (v[idx]==-1) return NULL;
+    Node *root=new Node(v[idx]);
+    root->left=buildTr(v);
+    root->right=buildTr(v);
+    return root;
+}
+
+void linetoline(Node *root) {
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty()) {
+        Node *curr=q.front();
+        if (curr==NULL) {
+            q.pop();
+            cout<<endl;
+            if (!q.empty()) q.push(NULL);
+        }
+        else {
+            cout<<curr->data<<" ";
+            q.pop();
+            if (curr->left!=NULL) q.push(curr->left);
+            if (curr->right!=NULL) q.push(curr->right);
+        }
+    }
+}
+
+void printTree(Node *root) {
+    if (root==NULL) return;
+    cout<<root->data<<" ";
+    printTree(root->left);
+    printTree(root->right);
+    return;
+}
+int diameter=INT_MIN;
+int height(Node *root) {
+    if (root==NULL) return 0;
+    int lh=height(root->left);
+    int rh=height(root->right);
+    diameter=max(diameter,lh+rh);
+    return max(lh,rh)+1;
+}
+
+int main() {
+    vector<int> v={1,2,-1,-1,3,4,5,6,-1,-1,-1,-1,7,-1,8,-1,9,-1,10,-1,-1};
+    Node* root=buildTr(v);
+    printTree(root);
+    cout<<endl;
+    cout<<height(root)<<endl;
+    cout<<diameter<<endl;
+    linetoline(root);
+    return 0;
+}
